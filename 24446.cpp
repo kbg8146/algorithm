d0d81c862b732ebba5de90975467fa2fc490e5dd
Bugeon Kim <kbg8146@gmail.com>
2026-02-14 23:57:53 +0900

    boj: 10989 count

--- 24446.cpp
+++ 24446.cpp
@@ -5,30 +5,34 @@
 using namespace std;
 
 int n,m,r;
-vector<vector<int>> v(n);
+vector<vector<int>> v;
 queue<int> q;
-int visited[100001];
+vector<int> depth(100001);
 
 void bfs(){
-    int cnt=1;
+    int cnt=0;
+    for(int i=1;i<=n;i++){
+        depth[i]=-1;
+    }
     q.push(r);
-    visited[r] = cnt;
+    depth[r] = 0;
     while(!q.empty()){
         int x = q.front();
         q.pop();
         for(int i=0;i<v[x].size();i++){
             int next = v[x][i];
-            if(!visited[next]){
+            if(depth[next]==-1){
                 q.push(next);
-                visited[next]=++cnt;
+                depth[next]=depth[x]+1;
             }
+            
         }
     }
 }
 
 int main(){
     cin >> n >> m >> r;
-
+    v.assign(n+1,vector<int> ());
     for(int i=0;i<m;i++){
         int a,b;
         cin >> a >> b;
@@ -37,8 +41,8 @@ int main(){
     }
     bfs();
 
-    for(int i=0;i<n;i++){
-        cout << visited[i] << "\n";
+    for(int i=1;i<=n;i++){
+        cout << depth[i] << "\n";
     }
 
     return 0;
