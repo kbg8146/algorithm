e71e364f659c9a56fba9ea33186165317594fb80
Bugeon Kim <kbg8146@gmail.com>
2026-02-15 14:08:13 +0900

    boj: 11651 sort

---
+++ 11651.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int,int>a,pair<int,int>b){
+    if(a.second==b.second){
+        return a.first < b.first;
+    }
+    return a.second<b.second;
+}
+int main(){
+
+    int n;
+    cin >> n;
+    vector<pair<int,int>> v;
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin >> x >> y;
+        v.push_back({x,y});
+    }
+    sort(v.begin(),v.end(),compare);
+    for(int i=0;i<n;i++){
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+    
+    return 0;
+}
