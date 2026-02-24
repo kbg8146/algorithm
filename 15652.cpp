1929df8bc5af1c1123a9cb81e3a029a4fa49d8c3
김부건 <kbg8146@gmail.com>
2026-02-24 11:58:23 +0900

    boj: 15652 backtrack

---
+++ 15652.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int arr[9];
+int visited[9];
+
+void backtrack(int depth,int start){
+
+    if(depth == m){
+        for(int i=0;i<m;i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=start;i<=n;i++){
+        arr[depth] = i;
+        backtrack(depth+1,i);
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    backtrack(0,1);
+    return 0;
+}
