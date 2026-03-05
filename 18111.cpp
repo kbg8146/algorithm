65e980b92f1e80eeff72fcfd5d4c2f9094b00f37
김부건 <kbg8146@gmail.com>
2026-03-05 17:17:16 +0900

    boj: 1811 bp

---
+++ 18111.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int n,m,b;
+vector<int> v;
+int main(){
+
+    cin >> n >> m >> b;
+    int origin_b = b;
+    v.assign(n*m,0);
+    for(int i=0;i<n*m;i++){
+        cin >> v[i];
+    }
+    int time; int height;
+    int min_time=INT_MAX;
+    for(int i=0;i<=256;i++){
+        time=0;
+        b=origin_b;
+        for(int j=0;j<n*m;j++){
+            
+            if(i>v[j]){
+                b=b-(i-v[j]);
+                time+=1*(i-v[j]);
+            }
+            else if(i<v[j]){
+                b=b+(v[j]-i);
+                time+=2*(v[j]-i);
+            }
+            
+        }
+        if(b<0) continue;
+        //cout <<"i: "<<i <<" time: "<< time << "\n";
+        min_time=min(min_time,time);
+        if(time==min_time) {
+            height=i;
+        }
+        
+    }
+    cout << min_time << " " << height;
+    return 0;
+}
+/*
+
+0부터 256까지 모두 해본다. for문
+모든 칸 을 i에 맞춰서 빼거나 더한다.
+
+if(i보다 작으면){
+    더하고
+    b에서 그만큼뺀다.
+    1초 증가
+}
+else(i보다 크면){
+    빼고
+    b에서 그만큼 더한다.
+    2초 증가
+}
+
+*/
