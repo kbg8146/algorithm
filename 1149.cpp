6c0ff332d6f60234f9cf7445f529a5dcd1b4f4a2
김부건 <kbg8146@gmail.com>
2026-03-23 17:48:37 +0900

    boj: 1149 nyet

---
+++ 1149.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int R[1001]={0};
+    int G[1001]={0};
+    int B[1001]={0};
+
+    for(int i=0;i<n;i++){
+        cin >> R[i] >> G[i] >> B[i];    
+    }
+
+    
+    return 0;
+}
