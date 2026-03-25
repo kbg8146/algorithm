16e56de95ea9567d8df08e02d5926a4e4e8f9f26
김부건 <kbg8146@gmail.com>
2026-03-25 12:03:03 +0900

    boj: 1446 dp

---
+++ 1446.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<pair<int,int>>> v;
+// 출발, 지름길, 도착점
+int main(){
+    int n, goal;
+    cin >> n >> goal;
+
+    int a, b, d;
+    v.resize(10001);
+    for(int i=0;i<n;i++){
+        cin >> a >> b >> d;
+        if(b-a<=d) continue;
+        v[b].push_back({a,d});
+    }
+    vector<int> dp(goal+1,0);
+    for(int i=1;i<=goal;i++){
+        dp[i] = dp[i-1] + 1;
+        for(auto& [start,dist] : v[i]){
+            dp[i] = min(dp[i],dp[start]+dist);
+        }
+    }
+    cout << dp[goal];
+
+    return 0;
+}
