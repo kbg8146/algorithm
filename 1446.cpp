dd03516fc3d7308d3d635e84cc5fd071749fb58d
김부건 <kbg8146@gmail.com>
2026-03-25 12:14:32 +0900

    boj: 1446 dp

--- 1446.cpp
+++ 1446.cpp
@@ -19,9 +19,11 @@ int main(){
     vector<int> dp(goal+1,0);
     for(int i=1;i<=goal;i++){
         dp[i] = dp[i-1] + 1;
-        for(auto& [start,dist] : v[i]){
-            dp[i] = min(dp[i],dp[start]+dist);
-        }
+        for(int j=0; j<v[i].size(); j++){
+            int start = v[i][j].first;
+            int dist = v[i][j].second;
+            dp[i] = min(dp[i], dp[start] + dist);
+        }   
     }
     cout << dp[goal];
 
