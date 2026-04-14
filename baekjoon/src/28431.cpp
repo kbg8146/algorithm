ae83bc657170ca78d960e63c9ae637c85f98d399
김부건 <kbg8146@gmail.com>
2026-04-14 20:29:48 +0900

    solved: 28431 XOR

--- baekjoon/src/28431.cpp
+++ baekjoon/src/28431.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-
-  int socks[6] = {0};
+  int result = 0;
   for (int i = 0; i < 5; i++) {
-    cin >> socks[i];
+    int x;
+    cin >> x;
+    result ^= x;
   }
-
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
-      if (i != j && socks[i] == socks[j]) {
-        socks[i] = 0;
-        socks[j] = 0;
-      }
-    }
-  }
-  for (int i = 0; i < 5; i++) {
-    if (socks[i] != 0) {
-      cout << socks[i];
-      return 0;
-    }
-  }
-  cout << 0;
+  cout << result << endl;
   return 0;
 }
